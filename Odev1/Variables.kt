aa7635723ab489127e428989253d916d0563673e
yusufcinaar <yusufcinar011@gmail.com>
2025-04-27 01:20:38 +0300

    Odev1: Değişken tanımlamaları ve çıktıları eklendi

---
+++ Odev1/Variables.kt
@@ -0,0 +1,51 @@
+fun main() {
+    // Değişken tanımlamaları
+    val ulke = "Türkiye"
+    val sehir = "İstanbul"
+    val telefon = "+90 555 123 4567"
+    val postaKodu = 34000
+    val email = "ornek@email.com"
+    val meslek = "Yazılımcı"
+    val stokMiktari = 150
+    val musteriAdi = "Ahmet Yılmaz"
+    val bakiye = 1500.50
+    val dogumGunu = "15.03.1990"
+    val maas = 25000.75
+    val medeniDurum = "Bekar"
+    val urunYorum = "Çok güzel bir ürün"
+    val odemeTarihi = "27.04.2025"
+    val odeme = 199.99
+    val siparisAdeti = 5
+    val arabaModeli = "Toyota Corolla"
+    val kitapAdi = "Kotlin Programming"
+    val yayinTarihi = "01.01.2025"
+    val indirimMiktari = 50.0
+    val musteriSoyadi = "Yılmaz"
+    val kargoTakipNo = "TR123456789"
+    val favoriRenk = "Mavi"
+
+    // Değişkenlerin çıktıları
+    println("Ülke: $ulke")
+    println("Şehir: $sehir")
+    println("Telefon: $telefon")
+    println("Posta Kodu: $postaKodu")
+    println("Email: $email")
+    println("Meslek: $meslek")
+    println("Stok Miktarı: $stokMiktari")
+    println("Müşteri Adı: $musteriAdi")
+    println("Bakiye: $bakiye TL")
+    println("Doğum Günü: $dogumGunu")
+    println("Maaş: $maas TL")
+    println("Medeni Durum: $medeniDurum")
+    println("Ürün Yorumu: $urunYorum")
+    println("Ödeme Tarihi: $odemeTarihi")
+    println("Ödeme: $odeme TL")
+    println("Sipariş Adeti: $siparisAdeti")
+    println("Araba Modeli: $arabaModeli")
+    println("Kitap Adı: $kitapAdi")
+    println("Yayın Tarihi: $yayinTarihi")
+    println("İndirim Miktarı: $indirimMiktari TL")
+    println("Müşteri Soyadı: $musteriSoyadi")
+    println("Kargo Takip No: $kargoTakipNo")
+    println("Favori Renk: $favoriRenk")
+}
